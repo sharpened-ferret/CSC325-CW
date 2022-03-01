e45824eaaaea4f28fc41d5216800682f1848b8ae
Neil Woodhouse <clonesupreme@gmail.com>
2022-03-01 16:34:54 +0000

    Added basic board rendering

--- cw1/8queens.py
+++ cw1/8queens.py
@@ -8,5 +8,22 @@ QUEEN_Y = (L_VAL % 8) + 1
 def main():
     print("Problem Started, Fixed Queen Position = ({}, {})\n".format(QUEEN_X, QUEEN_Y))
 
+    board = [
+        ['▢', '▨', '▢', '▨', '▢', '▨', '▢', '▨'],
+        ['▨', '▢', '▨', '▢', '▨', '▢', '▨', '▢'],
+        ['▢', '▨', '▢', '▨', '▢', '▨', '▢', '▨'],
+        ['▨', '▢', '▨', '▢', '▨', '▢', '▨', '▢'],
+        ['▢', '▨', '▢', '▨', '▢', '▨', '▢', '▨'],
+        ['▨', '▢', '▨', '▢', '▨', '▢', '▨', '▢'],
+        ['▢', '▨', '▢', '▨', '▢', '▨', '▢', '▨'],
+        ['▨', '▢', '▨', '▢', '▨', '▢', '▨', '▢']
+    ]
+
+    # Marks the position of the fixed piece with a black queen.
+    # Other queens will be white pieces to distinguish.
+    board[QUEEN_Y - 1][QUEEN_X - 1] = '♛'
+    for row in board:
+        print(''.join(row))
+
 if __name__ == "__main__":
     main()
