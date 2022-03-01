3f2ee648be85d2bad162cf59ed4ab94c0b9eaabd
Neil Woodhouse <clonesupreme@gmail.com>
2022-03-01 13:49:12 +0000

    Problem setup

---
+++ cw1/8queens.py
@@ -0,0 +1,12 @@
+# Neil Woodhouse - 851182
+
+K_VAL = 2
+L_VAL = 8
+QUEEN_X = (K_VAL % 8) + 1
+QUEEN_Y = (L_VAL % 8) + 1
+
+def main():
+    print("Problem Started, Queen Pos = ({}, {})".format(QUEEN_X, QUEEN_Y))
+
+if __name__ == "__main__":
+    main()
