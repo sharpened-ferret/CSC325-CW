71fc76ffd25dbb835331e3639a54a6d6614ec078
Neil Woodhouse <neiltwoodhouse@gmail.com>
2022-03-04 12:42:50 +0000

    Removed old attempt, renamed new version

--- cw1/8queens.py
+++ cw1/8queens.py
@@ -1,12 +1,15 @@
 # Neil Woodhouse - 851182
 
 import random
+import copy
 
  # Calculates the position for the fixed queen
 K_VAL = 2
 L_VAL = 8
 QUEEN_X = (K_VAL % 8) + 1
 QUEEN_Y = (L_VAL % 8) + 1
+
+QUEEN_ARRAY_POS = QUEEN_X - 1
 
  # Stores the position of the fixed queen as a tuple in the 0-7 coordinate space
 FIXED_QUEEN_ARR_POS = (QUEEN_X-1, QUEEN_Y-1)
@@ -15,11 +18,11 @@ FIXED_QUEEN_ARR_POS = (QUEEN_X-1, QUEEN_Y-1)
 BLACK_QUEEN = '♛'
 WHITE_QUEEN = '♕'
 
-def main():
-    print("Problem Started, Fixed Queen Position = ({}, {})\n".format(QUEEN_X, QUEEN_Y))
-    print("(Fixed queen Marked in Black)")
+ # Stops execution early once all solutions have been found
+TIMEOUT_VAL = 16
 
-    board = [
+ # An empty chessboard for use in rendering
+EMPTY_BOARD = [
         ['▢', '▨', '▢', '▨', '▢', '▨', '▢', '▨'],
         ['▨', '▢', '▨', '▢', '▨', '▢', '▨', '▢'],
         ['▢', '▨', '▢', '▨', '▢', '▨', '▢', '▨'],
@@ -29,139 +32,13 @@ def main():
         ['▢', '▨', '▢', '▨', '▢', '▨', '▢', '▨'],
         ['▨', '▢', '▨', '▢', '▨', '▢', '▨', '▢']
     ]
-
-    # Marks the position of the fixed piece with a black queen
-    board[QUEEN_Y - 1][QUEEN_X - 1] = BLACK_QUEEN
-
-    # Randomly assigns the other 7 queens, marked with white queens
-    starting_positions = []
-    unassigned_queens = 7
-    for x in range(0, 8):
-        if (x+1 != QUEEN_X):
-            rand_y = random.randint(0, 7)
-            board[rand_y][x] = WHITE_QUEEN
-            starting_positions.append((x, rand_y))
-            unassigned_queens -= 1
-    print_board(board)
-    print("Starting Positions (0-7 coords):" + str(starting_positions))
-    
-    # test_node = Node(None, [(2,1)])
-    # print(test_node.h)
-    # test_node.gen_children()
-    # print(test_node)
-    # convert_board(test_node.queen_positions)
-    # print("-------------")
-    # for child in test_node.children:
-    #     convert_board(child.queen_positions)
-    #     print("\n")
-
-
-    solution_path = astar(starting_positions)
-    if (solution_path is not None):
-        print("Safe Position Found...")
-        for node in solution_path:
-            print(node)
-    else:
-        print("No solutions found...")
-    
-
-
-def astar(queen_positions):
-    starting_node = Node(None, queen_positions)
-
-    open_list = []
-    closed_list = []
-
-    open_list.append(starting_node)
-
-    while(len(open_list) > 0):
-        curr_node = open_list[0]
-        open_list.remove(curr_node)
-        closed_list.append(curr_node)
-        print(str(curr_node.queen_positions) + "Num Threatened: " + str(curr_node.h))
-        convert_board(curr_node.queen_positions)
-
-         # If the current state has all Queens in safe positions, return it
-        if curr_node.h == 0:
-            move_path = []
-            curr = curr_node 
-            while curr is not None:
-                move_path.append(curr.queen_positions)
-                curr = curr.parent
-            return move_path[::-1]
-
-        curr_node.gen_children()
-        placeholder = []
-        improved_nodes = 0
-        for child in curr_node.children:
-            new_state = True
-            for closed_node in closed_list:
-                if child == closed_node:
-                    new_state = False
-            for open_node in open_list:
-                if child == open_node and child.g > open_node.g:
-                    new_state = False
-            if child.h >= curr_node.h: # TODO remove this test later
-                new_state = False 
-            if child.h < curr_node.h:
-                improved_nodes += 1
-            if new_state:
-                placeholder.append(child)
-        for child in placeholder:
-            if improved_nodes > 0:
-                if child.h < curr_node.h:
-                    open_list.append(child)
-            else:
-                open_list.append(child)
-
-        # if improved_nodes > 0:
-        #     for child in placeholder:
-        #         if child.h < curr_node.h:
-        #             open_list.append(child)
-        # else:
-        #     if len(placeholder) > 0:
-        #         open_list.append(placeholder[0])
-        open_list.sort(key=safe_queen_sort, reverse=False)
-        open_list_h = []
-        # for item in open_list:
-        #     open_list_h.append(item.h)
-        # print(open_list_h)
-        # print(closed_list)
-    
-def convert_board(path):
-    output_board = [
-        ['▢', '▨', '▢', '▨', '▢', '▨', '▢', '▨'],
-        ['▨', '▢', '▨', '▢', '▨', '▢', '▨', '▢'],
-        ['▢', '▨', '▢', '▨', '▢', '▨', '▢', '▨'],
-        ['▨', '▢', '▨', '▢', '▨', '▢', '▨', '▢'],
-        ['▢', '▨', '▢', '▨', '▢', '▨', '▢', '▨'],
-        ['▨', '▢', '▨', '▢', '▨', '▢', '▨', '▢'],
-        ['▢', '▨', '▢', '▨', '▢', '▨', '▢', '▨'],
-        ['▨', '▢', '▨', '▢', '▨', '▢', '▨', '▢']
-    ]
-    output_board[FIXED_QUEEN_ARR_POS[1]][FIXED_QUEEN_ARR_POS[0]] = BLACK_QUEEN
-    for pos in path:
-        output_board[pos[1]][pos[0]] = WHITE_QUEEN
-    print_board(output_board)
-
-
-def print_board(board):
-    output_board = board.copy()
-    output_board.reverse()
-    for row in output_board:
-        print(''.join(row))
-
-def safe_queen_sort(node):
-    return node.h
-
-def print_tuple(tuple_pos):
-    return "({}, {})".format(tuple_pos[0] + 1, tuple_pos[1] + 1)
 
 class Node:
-    def __init__(self, parent, queen_positions):
+    def __init__(self, parent, queen_positions, active_queen):
         self.parent = parent
         self.children = []
         self.queen_positions = queen_positions
+        self.active_queen = active_queen
 
         if parent is None:
             self.g = 0
@@ -177,24 +54,24 @@ class Node:
         return self.__str__()+"\n"
 
     def __eq__(self, other):
-        if len(self.children) == len(other.children):
-            num_matching = 0
-            for node in other.children:
-                if node in self.children:
-                    num_matching += 1
-            if num_matching == len(self.children):
-                return True
+        # if len(self.children) == len(other.children):
+        #     num_matching = 0
+        #     for node in other.children:
+        #         if node in self.children:
+        #             num_matching += 1
+        #     if num_matching == len(self.children):
+        #         return True
+        if self.queen_positions == other.queen_positions:
+            return True
         return False
 
      # The number of queens in safe positions is used as a success heuristic
     def threatened_queen_num(self):
         # TODO calculate number of safe queens
         safe_queen_num = 8
-        combined_queens = self.queen_positions.copy()
-        combined_queens.append(FIXED_QUEEN_ARR_POS)
-        for queen in combined_queens:
+        for queen in self.queen_positions:
             is_safe = True
-            other_queens = combined_queens.copy()
+            other_queens = copy.deepcopy(self.queen_positions)
             other_queens.remove(queen)
             for other in other_queens:
                  # Checks if another queen exists in the same row or col
@@ -218,54 +95,151 @@ class Node:
 
     def get_moves(self, curr_pos):
         valid_moves = []
-        blocked_positions = self.queen_positions.copy()
-        blocked_positions.remove(curr_pos)
-        blocked_positions.append(FIXED_QUEEN_ARR_POS)
         for n in range(0, 8):
-            # if n != curr_pos[0]:
-            #     valid_moves.append((n, curr_pos[1]))
             if n != curr_pos[1]:
                 valid_moves.append((curr_pos[0], n))
-        # for n in range(-7, 8):
-        #      # Calculates y=x diagonal moves
-        #     diagonal_pos = (curr_pos[0]+n, curr_pos[1]+n)
-        #     if (diagonal_pos[0] in range(0,8) and diagonal_pos[1] in range(0,8)):
-        #         valid_moves.append(diagonal_pos)
-
-        #      # Calculates y=-x diagonal moves
-        #     diagonal_pos = (curr_pos[0]+n, curr_pos[1]-n)
-        #     if (diagonal_pos[0] in range(0,8) and diagonal_pos[1] in range(0,8)):
-        #         valid_moves.append(diagonal_pos)
-        for pos in blocked_positions:
-            if pos in valid_moves:
-                valid_moves.remove(pos)
+        # print("Valid Moves: {}".format(valid_moves))
         return valid_moves
 
-    
-     # Checks if a solution has been found (if all queens are safe)
-    def is_solved(self):
-        if self.h == 0:
-            return True
-        return False
-
-    def gen_children(self):
+    def gen_children(self, active_queen):
         children = []
-         # TODO Check for all safe positions that children can be placed in
-        for i in range(len(self.queen_positions)):
-            curr_queen = self.queen_positions[i]
-            other_queens = self.queen_positions.copy()
-            other_queens.pop(i)
-            # TODO Generate children at possible positions
-            curr_moves = self.get_moves(curr_queen)
-            for move in curr_moves:
-                new_queen_positions = other_queens.copy()
-                new_queen_positions.append(move)
-                children.append(
-                    Node(self, new_queen_positions)
-                )
+        movable_queens = [0,1,3,4,5,6,7]
+        for i in movable_queens:
+            start_pos = self.queen_positions[i]
+            available_moves = self.get_moves(start_pos)
+            # print(i)
+
+            for move in available_moves:
+                # print("Current Move: {} for active queen: {}".format(move, active_queen))
+                new_queen_positions = copy.deepcopy(self.queen_positions)
+                # print(new_queen_positions)
+                new_queen_positions[i] = move
+                next_active = random.randint(0, 7)
+                if next_active == QUEEN_ARRAY_POS:
+                    next_active = (next_active + 1) % 8
+                children.append(Node(self, new_queen_positions, next_active))
         self.children = children
 
+ # Takes an array of tuples representing the positions of the non-fixed queens (in a 0-7 coordinate space)
+def astar(queen_positions):
+    starting_node = Node(None, queen_positions, 0)
 
+    open_list = []
+    closed_list = []
+    open_list.append(starting_node)
+
+    while len(open_list) > 0:
+        curr_node = open_list[0]
+        open_list.pop(0)
+        closed_list.append(curr_node)
+
+        print("Queen Positions={} Threatened Queens={}".format(curr_node.queen_positions, curr_node.h))
+
+        if curr_node.h == 0:
+            move_path = []
+            curr = curr_node
+            while curr.parent is not None:
+                move_path.append(curr.queen_positions)
+                curr = curr.parent
+            return move_path[::-1]
+        
+        curr_node.gen_children(curr_node.active_queen)
+        for child in curr_node.children:
+            new_state = True
+            if child in closed_list:
+                new_state = False
+            if child in open_list:
+                new_state = False
+            if new_state:
+                open_list.append(child)
+        open_list.sort(key=threatened_queen_sort, reverse=False)
+
+def astar_multi(queen_positions):
+    starting_node = Node(None, queen_positions, 0)
+    solution_nodes = []
+
+    open_list = []
+    closed_list = []
+    open_list.append(starting_node)
+
+    while len(open_list) > 0 and len(solution_nodes) < TIMEOUT_VAL:
+        curr_node = open_list[0]
+        open_list.pop(0)
+        closed_list.append(curr_node)
+
+        print("Queen Positions={}, Threatened Queens={}, Found Solutions={}".format(curr_node.queen_positions, curr_node.h, len(solution_nodes)))
+
+        if curr_node.h == 0:
+            solution_nodes.append(curr_node)
+        
+        curr_node.gen_children(curr_node.active_queen)
+        for child in curr_node.children:
+            new_state = True
+            if child in closed_list:
+                new_state = False
+            if child in open_list:
+                new_state = False
+            if new_state:
+                open_list.append(child)
+        open_list.sort(key=threatened_queen_sort, reverse=False)
+    return solution_nodes
+
+
+
+def print_tuple(tuple_pos):
+    return "({}, {})".format(tuple_pos[0] + 1, tuple_pos[1] + 1)
+
+def threatened_queen_sort(node):
+    return node.h
+
+def print_board(queen_positions):
+    output_board = copy.deepcopy(EMPTY_BOARD)
+    for pos in queen_positions:
+        output_board[pos[1]][pos[0]] = WHITE_QUEEN
+    output_board[FIXED_QUEEN_ARR_POS[1]][FIXED_QUEEN_ARR_POS[0]] = BLACK_QUEEN
+    output_board.reverse()
+    for row in output_board:
+        print(''.join(row))
+
+def main():
+    print("Problem Started, Fixed Queen Position = ({}, {})\n".format(QUEEN_X, QUEEN_Y))
+
+    starting_positions = [
+        None,
+        None, 
+        None,
+        None,
+        None,
+        None,
+        None,
+        None
+    ]
+    unassigned_queens = 7
+    for x in range(0, 8):
+        if (x != QUEEN_ARRAY_POS):
+            rand_y = random.randint(0, 7)
+            starting_positions[x] = (x, rand_y)
+            unassigned_queens -= 1
+    starting_positions[QUEEN_ARRAY_POS] = FIXED_QUEEN_ARR_POS
+    print("Starting Positions:")
+    for pos in starting_positions:
+        print("({}, {})".format(pos[0]+1, pos[1]+1))
+
+    move_path = astar(starting_positions)
+    if move_path is not None:
+        print("Single Solution Complete - Printing required moves:")
+        for move in move_path:
+            print_board(move)
+            print("\n")
+
+    print("Single Solution Complete, starting multiple solutions\n")  
+    multi_solutions = astar_multi(starting_positions)
+    if multi_solutions is not None:
+        print("All Solutions Found - Printing each final position.")
+        for solution in multi_solutions:
+            print_board(solution.queen_positions)
+            print("\n")
+    
 
 if __name__ == "__main__":
     main()
