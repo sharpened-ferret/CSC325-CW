2368e48fb431b63e9c426af1e1acd2cf8317692b
Neil Woodhouse <clonesupreme@gmail.com>
2022-03-30 13:12:19 +0100

    Split pluralisation inside verb phrases
    Passes tests 1-6

--- cw2/main.pl
+++ cw2/main.pl
@@ -4,7 +4,7 @@
 s(s(Tree1, Tree2)) --> np(Tree1, Pluralisation, Perspective), vp(Tree2, Pluralisation, Perspective).
 
 % Verb Phrase rules
-vp(vp(Tree1, Tree2), Pluralisation, Perspective) --> v(Tree1, Transitive, Pluralisation, Perspective), np(Tree2, Pluralisation, _).
+vp(vp(Tree1, Tree2), Pluralisation, Perspective) --> v(Tree1, Transitive, Pluralisation, Perspective), np(Tree2, Pluralisation2, _).
 
 % Verb Rules
 v(v(Word), Transitive, Pluralisation, Perspective) --> [Word], {lex(Word, Transitive, Pluralisation, Perspective)}.
