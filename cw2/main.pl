6121c0abc564c9ba59cba760c741e2c0ac325057
Neil Woodhouse <clonesupreme@gmail.com>
2022-04-02 14:51:25 +0100

    Added initial animacy check
    Needs testing

--- cw2/main.pl
+++ cw2/main.pl
@@ -1,37 +1,37 @@
 % This section will cover the initial production rules
 
 % Sentence Rules
-s(s(Tree1, Tree2)) --> np(Tree1, Pluralisation, Perspective, subject), vp(Tree2, Pluralisation, Perspective).
+s(s(Tree1, Tree2)) --> np(Tree1, Pluralisation, Perspective, subject, Animacy), vp(Tree2, Pluralisation, Perspective, Animacy).
 % s(s(Tree1, Tree2, Tree3)) --> np(Tree1, Pluralisation, Perspective, subject), pp(Tree2), vp(Tree3, Pluralisation, Perspective).
 %s(s(Tree1, Tree2, Tree3)) --> np(Tree1, Pluralisation, Perspective, subject), pp(Tree2), pp(Tree3), vp(Tree4, Pluralisation, Perspective).
 % s(s(Tree1, Tree2, Tree3)) --> np(Tree1, Pluralisation, Perspective, subject), vp(Tree2, Pluralisation, Perspective), pp(Tree3).
 % s(s(Tree1, Tree2, Tree3, Tree4)) --> np(Tree1, Pluralisation, Perspective, subject), pp(Tree2), vp(Tree3, Pluralisation, Perspective), pp(Tree4).
 
 % Verb Phrase rules
-vp(vp(Tree1, Tree2), Pluralisation, Perspective) --> v(Tree1, tv, Pluralisation, Perspective), np(Tree2, Pluralisation2, _, object).
-vp(vp(Tree), Pluralisation, Perspective) --> v(Tree, iv, Pluralisation, Perspective).
+vp(vp(Tree1, Tree2), Pluralisation, Perspective, Animacy) --> v(Tree1, tv, Pluralisation, Perspective, Animacy), np(Tree2, Pluralisation2, _, object, _).
+vp(vp(Tree), Pluralisation, Perspective, Animacy) --> v(Tree, iv, Pluralisation, Perspective, Animacy).
 
 % Verb Rules
-v(v(Word), Transitive, Pluralisation, Perspective) --> [Word], {lex(Word, Transitive, Pluralisation, Perspective)}.
+v(v(Word), Transitive, Pluralisation, Perspective, Animacy) --> [Word], {lex(Word, Transitive, Pluralisation, Perspective, Animacy)}.
 
 % Noun phrase rules
-np(np(Tree1, Tree2, Tree3), Pluralisation, 3, Argument) --> det(Tree1, Pluralisation), nbar(Tree2, Pluralisation), pp(Tree3).
-np(np(Tree1, Tree2), Pluralisation, 3, Argument) --> det(Tree1, Pluralisation), nbar(Tree2, Pluralisation).
+np(np(Tree1, Tree2, Tree3), Pluralisation, 3, Argument, Animacy) --> det(Tree1, Pluralisation), nbar(Tree2, Pluralisation, Animacy), pp(Tree3).
+np(np(Tree1, Tree2), Pluralisation, 3, Argument, Animacy) --> det(Tree1, Pluralisation), nbar(Tree2, Pluralisation, Animacy).
 
-np(np(Tree), Pluralisation, Perspective, Argument) --> pro(Tree, Pluralisation, Perspective, Argument).
-np(np(Tree1, Tree2), final, Pluralisation, 3, Argument) --> det(Tree1, Pluralisation), nbar(Tree2, Pluralisation).
+np(np(Tree), Pluralisation, Perspective, Argument, Animacy) --> pro(Tree, Pluralisation, Perspective, Argument).
+np(np(Tree1, Tree2), final, Pluralisation, 3, Argument, Animacy) --> det(Tree1, Pluralisation), nbar(Tree2, Pluralisation, Animacy).
 
 % Determiner rules
 det(det(Word), Pluralisation) --> [Word], {lex(Word, det, Pluralisation)}.
 
 % Nbar rules
-nbar(nbar(Tree), Pluralisation) --> n(Tree, Pluralisation).
-nbar(nbar(Tree1, Tree2), Pluralisation) --> jp(Tree1), n(Tree2, Pluralisation).
+nbar(nbar(Tree), Pluralisation, Animacy) --> n(Tree, Pluralisation, Animacy).
+nbar(nbar(Tree1, Tree2), Pluralisation) --> jp(Tree1), n(Tree2, Pluralisation, Animacy).
     % Note: I was unclear on how many adjectives should be permitted within a phrase, so here limited it to two per noun.
-nbar(nbar(Tree1, Tree2, Tree3), Pluralisation) --> jp(Tree1), jp(Tree2), n(Tree3, Pluralisation).
+nbar(nbar(Tree1, Tree2, Tree3), Pluralisation) --> jp(Tree1), jp(Tree2), n(Tree3, Pluralisation, Animacy).
 
 % Noun rules
-n(n(Word), Pluralisation) --> [Word], {lex(Word, n, Pluralisation)}.
+n(n(Word), Pluralisation, Animacy) --> [Word], {lex(Word, n, Pluralisation, Animacy)}.
 
 % Pronoun rules
 pro(pro(Word), Pluralisation, Perspective, Argument) --> [Word], {lex(Word, pro, Pluralisation, Perspective, Argument)}.
@@ -43,8 +43,8 @@ jp(jp(Tree)) --> adj(Tree).
 adj(adj(Word)) --> [Word], {lex(Word, adj)}.
 
 % Prepositional Phrase rules   TODO decide on pronoun prepositionals
-pp(pp(Tree1, Tree2)) --> prep(Tree1), np(Tree2, Pluralisation, 3, Argument). 
-pp(pp(Tree1, Tree2, pp(Tree3, Tree4))) --> prep(Tree1), np(Tree2, final, Pluralisation, 3, Argument), prep(Tree3), np(Tree4, _, 3, _).
+pp(pp(Tree1, Tree2)) --> prep(Tree1), np(Tree2, Pluralisation, 3, Argument, Animacy). 
+pp(pp(Tree1, Tree2, pp(Tree3, Tree4))) --> prep(Tree1), np(Tree2, final, Pluralisation, 3, Argument, Animacy), prep(Tree3), np(Tree4, _, 3, _, Animacy).
 
 % Preposition rules
 prep(prep(Word)) --> [Word], {lex(Word, prep)}.
@@ -71,26 +71,26 @@ lex(you,pro,plural,2,object).
 lex(them,pro,plural,3,object).
 
 % Verbs
-lex(know,tv,singular,1).
-lex(know,tv,singular,2).
-lex(knows,tv,singular,3).
-lex(know,tv,plural,_).
-lex(see,tv,singular,1).
-lex(see,tv,singular,2).
-lex(sees,tv,singular,3).
-lex(see,tv,plural,_).
-lex(hire,tv,singular,1).
-lex(hire,tv,singular,2).
-lex(hires,tv,singular,3).
-lex(hire,tv,plural,_).
-lex(fall,iv,singular,1).
-lex(fall,iv,singular,2).
-lex(falls,iv,singular,3).
-lex(fall,iv,plural,_).
-lex(sleep,iv,singular,1).
-lex(sleep,iv,singular,2).
-lex(sleeps,iv,singular,3).
-lex(sleep,iv,plural,_).
+lex(know,tv,singular,1,animate).
+lex(know,tv,singular,2,animate).
+lex(knows,tv,singular,3,animate).
+lex(know,tv,plural,_,animate).
+lex(see,tv,singular,1,animate).
+lex(see,tv,singular,2,animate).
+lex(sees,tv,singular,3,animate).
+lex(see,tv,plural,_,animate).
+lex(hire,tv,singular,1,animate).
+lex(hire,tv,singular,2,animate).
+lex(hires,tv,singular,3,animate).
+lex(hire,tv,plural,_,animate).
+lex(fall,iv,singular,1,_).
+lex(fall,iv,singular,2,_).
+lex(falls,iv,singular,3,_).
+lex(fall,iv,plural,_,_).
+lex(sleep,iv,singular,1,animate).
+lex(sleep,iv,singular,2,animate).
+lex(sleeps,iv,singular,3,animate).
+lex(sleep,iv,plural,_,animate).
 
 % Determiners
 lex(the,det,_).
@@ -98,16 +98,16 @@ lex(a,det,singular).
 lex(two,det,plural).
 
 % Nouns
-lex(man,n,singular).
-lex(woman,n,singular).
-lex(apple,n,singular).
-lex(chair,n,singular).
-lex(room,n,singular).
-lex(men,n,plural).
-lex(women,n,plural).
-lex(apples,n,plural).
-lex(chairs,n,plural).
-lex(rooms,n,plural).
+lex(man,n,singular,animate).
+lex(woman,n,singular,animate).
+lex(apple,n,singular,inanimate).
+lex(chair,n,singular,inanimate).
+lex(room,n,singular,inanimate).
+lex(men,n,plural,animate).
+lex(women,n,plural,animate).
+lex(apples,n,plural,inanimate).
+lex(chairs,n,plural,inanimate).
+lex(rooms,n,plural,inanimate).
 
 % Prepositions
 lex(on,prep).
